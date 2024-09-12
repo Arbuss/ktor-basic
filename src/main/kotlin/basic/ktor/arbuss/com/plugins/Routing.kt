d9c3976eb64ab830be577326e1f651b3630ce142
Arbuss <buss.art7@gmail.com>
2024-09-12 16:52:10 +0300

    initial

---
+++ src/main/kotlin/basic/ktor/arbuss/com/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package basic.ktor.arbuss.com.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
